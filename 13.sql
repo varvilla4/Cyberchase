7954ecd5465f08feecc148baf7f8feb075ece051
Vianey Arvilla <62896183+varvilla4@users.noreply.github.com>
2024-09-30 17:58:20 -0600

    Add files via upload

---
+++ 13.sql
@@ -0,0 +1,3 @@
+SELECT "title", "topic" FROM "episodes"
+WHERE "title" LIKE 'A%'
+AND "air_date" BETWEEN '2002-01-01' AND '2002-12-31';
---
+++ 6.sql
@@ -0,0 +1,4 @@
+SELECT "title" FROM "episodes"
+WHERE "season" = 6
+AND "air_date" < '2008-01-01';
+
